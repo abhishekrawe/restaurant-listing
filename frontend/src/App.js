45e4bd52c70ddcd51f8b9d8f6ae8fa168b7201b6
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-11-05 13:11:24 +0530

    Add Router Dom /addlist /updateList Restaurant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Restaurant from "./pages/Restaurant";
+import AddList from "./pages/AddList";
+import UpdateList from "./pages/UpdateList";
 
 function App() {
   return (
     <div className="App">
-      <h2> Hello </h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Restaurant />} />
+          <Route path="/addlist" element={<AddList />} />
+          <Route path="/updatelist" element={<UpdateList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/UpdateList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UpdateList() {
+  return <div>UpdateList</div>;
+}
+
+export default UpdateList;
