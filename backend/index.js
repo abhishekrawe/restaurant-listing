f35abbc22d5b31acaec43df08e20ff8e8ae964a1
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-11-05 12:14:47 +0530

    ✨Get /resturent data from db

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,14 @@ app.get("/", (req,res)=> {
    res.json("Welcome To My Backend");
 })
 
+app.get("/restaurant", (req, res) => {
+  const q = "SELECT * FROM restaurant";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
 app.listen(5500, () => {
     console.log("Welcome To My Backend");
 })
