b3edd2df9bcf897e8ba80765b4e35d6960850cce
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-11-05 14:31:08 +0530

    ✨Fetched Data to Frontend From db using Axios

--- frontend/src/pages/Restaurant.jsx
+++ frontend/src/pages/Restaurant.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 
 function Restaurant() {
-  return <div>Restaurant</div>;
+  const [restaurant, setRestaurant] = useState([]);
+
+  useEffect(() => {
+    const fetchAllRestaurantList = async () => {
+      try {
+        const res = await axios.get("http://localhost:5500/restaurant")
+        //console.log(res)
+        setRestaurant(res.data);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchAllRestaurantList()
+  }, [])
+
+  return <div>
+    <h1>Resturant</h1>
+    <div className="restaurant">
+      {restaurant.map(restaurant => (
+        <div className="restaurant" key={restaurant.id}>
+          {restaurant.picture && <img src={restaurant.picture} alt="" />}
+          <h2>{restaurant.name}</h2>
+          <h2>{restaurant.address}</h2>
+          <h2>{restaurant.contact}</h2>
+        </div>
+      ))}
+    </div>
+
+  </div>;
 }
 
 export default Restaurant;
