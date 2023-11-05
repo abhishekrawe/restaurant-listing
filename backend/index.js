33aa64f4d14bcfc9c186e037ad9cac1b3c2a25fc
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-11-05 12:37:01 +0530

    ⚡Post /restaurant data & add exp server midleware

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
-import express from  "express"
-import mysql from "mysql"
+import express from "express";
+import mysql from "mysql";
 
-
-const app = express()
+const app = express();
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"pass123",
-    database:"test"
-})
+  host: "localhost",
+  user: "root",
+  password: "pass123",
+  database: "test",
+});
 
-app.get("/", (req,res)=> {
-   res.json("Welcome To My Backend");
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Welcome To My Backend");
+});
 
 app.get("/restaurant", (req, res) => {
   const q = "SELECT * FROM restaurant";
@@ -23,6 +24,22 @@ app.get("/restaurant", (req, res) => {
   });
 });
 
+app.post("/restaurant", (req, res) => {
+  const q =
+    "INSERT INTO restaurant (`name`,`address`, `contact`, `picture`) VALUES (?)";
+  const values = [
+    req.body.name,
+    req.body.address,
+    req.body.contact,
+    req.body.picture,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Resturant has created succefully🎉");
+  });
+});
+
 app.listen(5500, () => {
-    console.log("Welcome To My Backend");
-})
+  console.log("Welcome To My Backend");
+});
