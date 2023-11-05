c485dd06e608873690446a9c5748510c8bc8fa1a
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-11-05 12:10:39 +0530

    ⚡Created Connection Using MYSQL

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 import express from  "express"
+import mysql from "mysql"
+
 
 const app = express()
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"pass123",
+    database:"test"
+})
+
+app.get("/", (req,res)=> {
+   res.json("Welcome To My Backend");
+})
 
 app.listen(5500, () => {
     console.log("Welcome To My Backend");
